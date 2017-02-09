4a7a496a27a51b06d276bce7c8d7b723e4b73d80
Philip White <phil_23@live.co.uk>
2017-02-09 14:17:33 +0000

    added basic get routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+def index
+  @name = "Index action"
+end
+
+def new
+  @name = "New action"
+end
+
+def edit
+  @name = "Edit action"
+end
+
+def show
+  @name = "Show action"
+end
+end
