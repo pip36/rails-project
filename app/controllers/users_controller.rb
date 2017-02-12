dc88638ae9bb98c95fae0d64a567d9011a25bb66
Philip White <phil_23@live.co.uk>
2017-02-12 19:09:59 +0000

    added sign up page skeleton

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,5 @@
 class UsersController < ApplicationController
-
-def index
-  @name = "Index action"
-end
-
-def new
-  @name = "New action"
-end
-
-def edit
-  @name = "Edit action"
-end
-
-def show
-  @name = "Show action"
-end
+  def new
+    @name = "Signup"
+  end
 end
