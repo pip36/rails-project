209f4389669da4aa1ccf5897ba7d1253538ae250
Philip White <phil_23@live.co.uk>
2017-02-13 19:05:14 +0000

    added create new user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
   def new
     @name = "Signup"
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Hi #{@user.name}, welcome to the sample app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
