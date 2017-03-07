001f1d5af9dfd95630a7fbb55b5252d6f484d1c9
Philip White <phil_23@live.co.uk>
2017-03-07 10:45:44 +0000

    removed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'admin', email: 'admin@admin.com', password: 'AdminPassword', admin: true)
+#User.create(name: 'admin', email: 'admin@admin.com', password: 'AdminPassword', admin: true)
 
 
 
