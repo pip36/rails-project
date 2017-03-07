c5c43485c1a18be715a77c0391d4f8ffdb04b643
Philip White <phil_23@live.co.uk>
2017-03-07 12:45:17 +0000

    fixed email host

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,7 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account Activation"
+    
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
